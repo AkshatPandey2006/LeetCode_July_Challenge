41eab93599655d503f88d217f9a2a96e6e21b4f3
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-07-06 13:00:02 +0530

    Update Pass_the_Pillow.cpp

--- Pass_the_Pillow.cpp
+++ Pass_the_Pillow.cpp
@@ -1,10 +1,7 @@
-class Solution {
-public:
-    int passThePillow(int n, int time) {
-        n--;
-        int rem = time % n;
-        int quo = time / n;
-        if(quo % 2 != 0) return (n - rem) + 1;
-        return rem + 1;
-    }
-};
+int passThePillow(int n, int time) {
+    n--;
+    int rem = time % n;
+    int quo = time / n;
+    if(quo % 2 != 0) return (n - rem) + 1;
+    return rem + 1;
+}
